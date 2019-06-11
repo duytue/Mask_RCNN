99a0a64ec9a8be6107cb8d95bd528d423a5ee526
Duy-Tue Tran-Van <duytue289@gmail.com>
2019-06-11 16:30:16 +0700

    change queue size to reduce memory

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2571,7 +2571,7 @@ class MaskRCNN():
             callbacks=callbacks,
             validation_data=val_generator,
             validation_steps=self.config.VALIDATION_STEPS,
-            max_queue_size=100,
+            max_queue_size=64,
             workers=workers,
             use_multiprocessing=True,
         )
